5e80950ac0fead72e2ea136b6b756b97a647828b
Sanjida Akter Nipa <sanjidanipa2004@gmail.com>
2025-09-08 23:59:59 +0600

    category style

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const allCategories = () => {
     displayAllPlants(data.plants)
 };
 
+const plantCategory =
+
 allPlants()
 
 const displayAllPlants = (plants) =>{
@@ -19,13 +21,13 @@ const displayAllPlants = (plants) =>{
         const plantCard = document.createElement("div")
         plantCard.innerHTML = `
         <div class="p-4 bg-white shadow-lg rounded-lg">
-        <img src="${plant.images}">
+        <img src="${plant.image}" class="rounded-lg w-80 h-44 mb-3">
         <div class="space-y-1">
           <h2 class="font-semibold text-[#1F2937]">${plant.name}</h2>
           <p class="text-[#1F2937]">${plant.description}</p>
         </div>
         <div class="flex justify-between items-center my-3">
-          <p class="p-2 bg-[#DCFCE7] text-[#15803D] font-medium rounded-xl">${plant.category}</p>
+          <p class=" p-2 bg-[#DCFCE7] text-[#15803D] font-medium rounded-xl">${plant.category}</p>
           <p class="font-semibold">৳<span>${plant.price}</span></p>
         </div>
         <button id="add_cart" class="btn bg-[#15803D] text-white rounded-2xl w-full ">Add to Cart</button>
@@ -41,7 +43,7 @@ const displayCategoriesName = (names) => {
         for (let name of names ){
             const btnDiv = document.createElement("div")
             btnDiv.innerHTML = `
-            <p id= "category_btn_${name.category_name}" onclick="categoriesDetail(${name.category_name})" class="hover:bg-[#15803D] rounded-md hover:text-xl hover:font-semibold hover:text-white w-full p-3 my-3">${name.category_name}</p> `
+            <button id= "category_btn_${name.category_name}" onclick="categoriesDetail(${name.category_name})" class="btn bg-transparent border-none flex justify-start hover:bg-[#15803D] rounded-md hover:text-xl hover:font-semibold hover:text-white w-full p-3 my-3">${name.category_name}</button> `
             categoriesName.appendChild(btnDiv)
         }
 }
